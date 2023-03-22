06224a6f591e2a9fe8485c0652e0597f8998d164
Andrew Subotin <subotinandrey5@gmail.com>
2023-03-22 11:08:17 +0200

    Extended User model to add profile pic

--- articles/forms.py
+++ articles/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = CustomUser
+        fields = ['username', 'email', 'password1',
+                  'password2', 'profile_image']
