b7a0a77084f29f37b10084a0675f7deb027e615c
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-05 12:15:38 +0300

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -43,7 +43,7 @@ def password_reset_request(request):
                     email = render_to_string(email_template_name, c)
                     try:
                         send_mail(subject, email,
-                                  'admin@gmail.com', [user.email], fail_silently=False)
+                                  'admin1@gmail.com', [user.email], fail_silently=False)
                     except BadHeaderError:
                         return HttpResponse('Invalid header found.')
                     return redirect("/password_reset/done/")
