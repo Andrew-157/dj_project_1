88a13c0d3fa979eb1b578fd0cc18e6c824e5afd0
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-19 10:23:51 +0300

    Update settings.py

--- articlee/settings.py
+++ articlee/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'articles.CustomUser'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
